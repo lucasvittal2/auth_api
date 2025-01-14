eb1211a176fa1960ea36bd01f18978448d1fdba8
lucasvittal2 <lucasvittal@gmail.com>
2025-01-14 09:17:11 -0300

    feat: implemented signup endpoint.
    the following tests were made manually:
    
    - test if user which userid and username is note being used is created with success
    
    - test if user with an existent user_id is blocked
    - teste if user with an existent username is blocked

---
+++ src/auth_api/app/api.py
@@ -0,0 +1,105 @@
+import logging
+from datetime import datetime, timedelta
+
+import pytz
+import yaml
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+from auth_api.app.authentication import Authenticator
+from auth_api.app.models import *
+from auth_api.databases.mongo import MongoHandler
+from auth_api.utils.read_files import read_yaml
+
+# Instance objects globally used
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app_configs = read_yaml("app_configs.yaml")
+app_name = app_configs["APP_NAME"]
+time_zone = pytz.timezone(app_configs["TIME_ZONE"])
+mongo = MongoHandler(**app_configs["AUTH_DB"])
+auth_config = AuthConfig(**app_configs["AUTH_CONFIG"])
+authenticator = Authenticator(auth_config)
+
+
+# logging setup
+# Logging setup
+
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=f"%(asctime)s - [{app_name}] - %(levelname)s:  %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler()],
+)
+
+# Define API Endpoints
+
+
+@app.post("/auth-api/v1/signup")
+def singup(body_request: RegisterRequest) -> JSONResponse:
+    try:
+        mongo.create_collection_if_not_exist(body_request.app_name)
+        hashed_password = authenticator.hash_password(body_request.password)
+        expire = datetime.now(tz=time_zone) + timedelta(hours=1)
+        payload = RegisterPayload(
+            **body_request.model_dump(), expire=expire.strftime("%Y-%m-%d %H:%M:%S")
+        )
+        token = authenticator.create_jwt_token(payload)
+        document = {
+            "user_id": body_request.user_id,
+            "user_name": body_request.username,
+            "password": hashed_password,
+            "token": token,
+            "role": body_request.role,
+        }
+        username_exists = mongo.get_document(
+            body_request.app_name, filter_query={"user_name": body_request.username}
+        )
+        userid_used = mongo.get_document(
+            body_request.app_name, filter_query={"user_id": body_request.user_id}
+        )
+        print(userid_used)
+
+        if userid_used:
+            response = JSONResponse(
+                content={
+                    "status": "FAILED",
+                    "message": "User id already being used, use another user id!",
+                },
+                status_code=status.HTTP_403_FORBIDDEN,
+            )
+
+        elif not username_exists:
+            mongo.create(body_request.app_name, document)
+            response = JSONResponse(
+                content={"status": "SUCCESS"}, status_code=status.HTTP_200_OK
+            )
+            logging.info("new user recorded successfully !")
+
+        else:
+            response = JSONResponse(
+                content={
+                    "status": "FAILED",
+                    "message": "User with this name already exist ! Choose another username.",
+                },
+                status_code=status.HTTP_403_FORBIDDEN,
+            )
+
+    except Exception as err:
+        response = JSONResponse(
+            content={"status": "FAILED", "message": f" sign up failed due to {err}"},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+    return response
